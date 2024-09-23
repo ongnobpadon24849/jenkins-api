ca1be892e2bf4ab164ad30a475d40893e750a297
Noppadon Panboonyeun <ongnobpadon24849@gmail.com>
2024-09-23 19:41:56 +0700

    Edit GETCODE

--- flask_api/app.py
+++ flask_api/app.py
@@ -8,7 +8,7 @@ def index():
 
 @app.route('/getcode', methods=['GET'])
 def getcode():
-    return "SDPX GROUP 3 GET CODE 200 OK?"
+    return "SDPX GROUP 3 (GETCODE) Date 2021-09-30"
 
 
 @app.route('/plus/<num1>/<num2>', methods=['GET'])
